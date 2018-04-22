cbd4ec80bfb2120ed5a20c012a4accfcca539c63
Amrir Toufali <Amrir@HandsomeMedia.com>
2018-04-22 12:38:09 -0700

    refactor threshold function

--- src/index.js
+++ src/index.js
@@ -4,22 +4,20 @@ import './logo.svg'
 const header = document.querySelector('header')
 const sentinel = document.createElement('span')
 
-const intOpts = {
-  threshold: ((arr, i) => {
-    while (i--) {
-      arr[i] = i / 100
-    }
-    return arr
-  })([], 100)
-}
-
 const render = function(){
   header.style.transform = `translateY(${-window.scrollY * .5}px)`
 }
 
-const intObs = new IntersectionObserver(render, intOpts);
+const thresholdArr = function(num){
+  const arr = []
+  while (num--) {
+    arr[num] = num / 100
+  }
+  return arr
+}
 
-sentinel.className = 'sentinel'
+const intersectObs = new IntersectionObserver(render, {threshold: thresholdArr(100)});
+
 sentinel.style = `position: absolute; top: 0; width: 1px; height: ${header.clientHeight}px`
 document.body.appendChild(sentinel)
-intObs.observe(sentinel);
+intersectObs.observe(sentinel);
